4009b9b9702f615f3858d9a288a323234ea85343
Seohui Park <parkseohuinim@gmail.com>
2022-09-28 16:50:34 +0900

    feat: add method for single linked list class
    add, delete node method

--- src/datastructures/SingleLinkedList.java
+++ src/datastructures/SingleLinkedList.java
@@ -24,6 +24,53 @@ public class SingleLinkedList<T> {
         }
     }
 
+    public Node<T> searchNode(T data) {
+        if (this.head != null) {
+            Node<T> node = this.head;
+            while (node.data != null) {
+                if (node.data == data) {
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void addNodeInside(T data, T isData) {
+        Node<T> searchedNode = this.searchNode(isData);
+
+        if (searchedNode == null) {
+            this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            searchedNode.next = new Node<T>(data);
+            searchedNode.next.next = nextNode;
+        }
+    }
+
+    public boolean deleteNode(T isData) {
+        if (this.head == null) {
+            return false;
+        } else {
+            Node<T> node = this.head;
+            if (node.data == isData) {
+                this.head = this.head.next;
+                return true;
+            } else {
+                while (node.next != null) {
+                    if (node.next.data == isData) {
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+    }
+
     public void printAll() {
         if (head != null) {
             Node<T> node = this.head;
@@ -40,6 +87,8 @@ public class SingleLinkedList<T> {
         linkedList.addNode(1);
         linkedList.addNode(2);
         linkedList.addNode(3);
+        linkedList.addNodeInside(4, 1);
+        linkedList.deleteNode(2);
         linkedList.printAll();
     }
 }
